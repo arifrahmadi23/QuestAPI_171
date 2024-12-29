9ad7e12b7ab9db484087d41c49474f452e19d913
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-29 15:30:14 +0700

    Membuat object destinasi untuk insert view

---
+++ app/src/main/java/com/example/pertemuan13/ui/view/InsertView.kt
@@ -0,0 +1,8 @@
+package com.example.pertemuan13.ui.view
+
+import com.example.pertemuan13.navigation.DestinasiNavigasi
+
+object DestinasiEntry : DestinasiNavigasi{
+    override val route = "item_entry"
+    override val titleRes = "Entry Mhs"
+}
