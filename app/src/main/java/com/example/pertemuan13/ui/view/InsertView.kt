df705acb10dc2d35f3882672d4f7582b6df37341
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-29 16:31:20 +0700

    Membuat Fungsi EntryMhsScreen pada InsertView

--- app/src/main/java/com/example/pertemuan13/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan13/ui/view/InsertView.kt
@@ -4,21 +4,29 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
 import androidx.compose.material3.Divider
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan13.customwidget.CostumeTopAppBar
 import com.example.pertemuan13.navigation.DestinasiNavigasi
 import com.example.pertemuan13.ui.viewmodel.InsertUiEvent
 import com.example.pertemuan13.ui.viewmodel.InsertUiState
 import com.example.pertemuan13.ui.viewmodel.InsertViewModel
 import com.example.pertemuan13.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.launch
 
 object DestinasiEntry : DestinasiNavigasi{
     override val route = "item_entry"
@@ -32,7 +40,34 @@ fun EntryMhsScreen(
     modifier: Modifier = Modifier,
     viewModel: InsertViewModel = viewModel(factory = PenyediaViewModel.Factory)
 ){
-
+    val coroutineScope = rememberCoroutineScope()
+    val scrollBehavior = TopAppBarDefaults.enterAlwaysScrollBehavior()
+    Scaffold(
+        modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
+        topBar = {
+            CostumeTopAppBar(
+                title = DestinasiEntry.titleRes,
+                canNavigateBack = true,
+                scrollBehavior = scrollBehavior,
+                navigateUp = navigateBack
+            )
+        }
+    ) { innerPadding ->
+        EntryBody(
+            insertUiState = viewModel.uiState,
+            onMahasiswaValueChange = viewModel::updateInsertMhsState,
+            onSaveClick = {
+                coroutineScope.launch {
+                    viewModel.insertMhs()
+                    navigateBack()
+                }
+            },
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(rememberScrollState())
+                .fillMaxWidth()
+        )
+    }
 }
 
 @Composable
