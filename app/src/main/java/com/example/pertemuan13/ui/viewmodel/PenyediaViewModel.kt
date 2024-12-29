e802d433509241a13a97fc689e0611cf5634721a
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-29 15:35:41 +0700

    Membuat object PenyediaViewModel

---
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/PenyediaViewModel.kt
@@ -0,0 +1,19 @@
+package com.example.pertemuan13.ui.viewmodel
+
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.viewmodel.CreationExtras
+import androidx.lifecycle.viewmodel.initializer
+import androidx.lifecycle.viewmodel.viewModelFactory
+import coil.decode.BitmapFactoryDecoder
+import com.example.pertemuan13.MahasiswaApplications
+import okhttp3.EventListener
+
+object PenyediaViewModel{
+    val Factory = viewModelFactory {
+        initializer { HomeViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+        initializer { InsertViewModel(aplikasiMahasiswa().container.mahasiswaRepository) }
+    }
+}
+
+fun CreationExtras.aplikasiMahasiswa(): MahasiswaApplications =
+    (this[ViewModelProvider.AndroidViewModelFactory.APPLICATION_KEY] as MahasiswaApplications)
