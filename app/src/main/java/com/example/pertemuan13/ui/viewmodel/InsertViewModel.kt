29b148ac88a3e406342b220bbee2b1460187ceb9
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-29 14:57:43 +0700

    Membuat Data class InsertUiState

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
@@ -5,6 +5,10 @@ import com.example.pertemuan13.model.Mahasiswa
 class InsertViewModel {
 }
 
+data class InsertUiState(
+    val insertUiEvent: InsertUiEvent = InsertUiEvent()
+)
+
 data class InsertUiEvent(
     val nim: String ="",
     val nama: String ="",
@@ -15,5 +19,26 @@ data class InsertUiEvent(
 
 )
 
+fun InsertUiEvent.toMhs():Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
+
+fun Mahasiswa.toUiStateMhs():InsertUiState = InsertUiState(
+    insertUiEvent = toInsertUiEvent()
+)
+
+fun Mahasiswa.toInsertUiEvent(): InsertUiEvent = InsertUiEvent(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    jenisKelamin = jenisKelamin,
+    kelas = kelas,
+    angkatan = angkatan
+)
 
 
