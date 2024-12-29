ed85d6fb39ac9e3b3f5f14a28767e87bd08802eb
arifrahmadi23 <muhammad231arif@gmail.com>
2024-12-29 15:02:13 +0700

    Membuat class InsertViewModel

--- app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan13/ui/viewmodel/InsertViewModel.kt
@@ -1,8 +1,31 @@
 package com.example.pertemuan13.ui.viewmodel
 
+import android.health.connect.datatypes.ExerciseRoute
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.pertemuan13.model.Mahasiswa
+import com.example.pertemuan13.repository.MahasiswaRepository
+import kotlinx.coroutines.launch
 
-class InsertViewModel {
+class InsertViewModel (private val mhs: MahasiswaRepository):ViewModel(){
+        var uiState by mutableStateOf(InsertUiState())
+            private set
+
+    fun updateInsertMhsState(insertUiEvent: InsertUiEvent){
+        uiState = InsertUiState(insertUiEvent = insertUiEvent)
+    }
+    suspend fun insertMhs(){
+        viewModelScope.launch {
+            try {
+                mhs.insertMahasiswa(uiState.insertUiEvent.toMhs())
+            }catch (e : Exception){
+                e.printStackTrace()
+            }
+        }
+    }
 }
 
 data class InsertUiState(
